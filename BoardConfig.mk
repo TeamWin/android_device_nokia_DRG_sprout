acc4360350cb3dfc7ddf868185de33cf658036ae
RaghuVarma331 <raghuvarma331@gmail.com>
2022-12-06 08:52:45 +0000

    DRG_sprout: drop libicui18n from recovery modules
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     ashmemd_aidl_interface-cpp \
     libashmemd_client \
     libcap \
-    libicui18n \
     libion \
     libandroidicu \
     libicuuc \
@@ -117,7 +116,6 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpcrecpp.so \
